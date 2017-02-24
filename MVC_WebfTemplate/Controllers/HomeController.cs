106322cc4801f78b0106dfedb7459ca28ff7e51c
Peter Pan <Peter Pan@PETERPAN-OMEN>
2017-02-24 10:36:52 +0900

    HOme Controller  수정

--- MVC_WebfTemplate/Controllers/HomeController.cs
+++ MVC_WebfTemplate/Controllers/HomeController.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
 
 namespace MVC_WebfTemplate.Controllers
 {
     public class HomeController : ApiController
     {
         public HomeController()
+        {
+            
+        }
+
+        public ActionResult Index()
         {
             return this.View();
         }
